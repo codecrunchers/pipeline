3cdfc626e89451a7dc70672654ab0fa2ce060b43
Alan <alan@codecrunchers.ie>
2016-11-18 00:30:52 +0000

    ready for chef

---
+++ docker-ci-tool-stack/chef/rxp-pipline/metadata.rb
@@ -0,0 +1,15 @@
+name 'rxp-pipline'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures rxp-pipline'
+long_description 'Installs/Configures rxp-pipline'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/rxp-pipline/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/rxp-pipline' if respond_to?(:source_url)
--- docker-ci-tool-stack/run.sh
+++ docker-ci-tool-stack/run.sh
@@ -1,17 +1,9 @@
-!#/bin/bash
-
-PROJNAME=$1
-
-mkdir gitlab
-cd gitlab
-mkdir "$PROJNAME"
-cd "$PROJNAME"
-cp ../../../demo/ . -R
-cd demo
-git init
-git add .
-git commit -am "II"
-git remote add origin http://gitlab/root/$PROJNAME.git
-
-
-
+#!/bin/bash
+trimmed=`echo $1 | tr -d ' '`
+PROJNAME=$trimmed
+__pwd="$PWD"
+mkdir -p "/home/alan/devel/docker-volumes/gitlab/data/git-data/repositories/root/"
+cd "/home/alan/devel/docker-volumes/gitlab/data/git-data/repositories/root/"
+git clone --bare "$__pwd/demo/" "$PROJNAME.git"
+chown 200 /home/alan/devel/docker-volumes/gitlab/ -R
+chown 200 /home/alan/devel/docker-volumes/gitlab/* -R
